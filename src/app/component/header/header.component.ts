af09f0b943b506edd5021aa5372c23235cba06f7
unknown <bhakti@prissoft.net>
2022-06-15 16:33:19 +0530

    changes done

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderService } from 'src/app/header/header.service';
 import { AuthenticationService } from 'src/app/service/authentication.service';
@@ -14,13 +15,17 @@ export class HeaderComponent implements OnInit{
   userToken!: any;
   role!: string;
   user: any;
+  elem!: any;
+  isFullScreen! : boolean;
   constructor(private router: Router, 
     public headerService: HeaderService,
     private authenticationService: AuthenticationService,
     private userService: UserService,
-    private sharedHeaderService: SharedHeaderService) { }
+    private sharedHeaderService: SharedHeaderService,
+    @Inject(DOCUMENT) private document: any) { }
 
   ngOnInit(): void {
+    this.elem = document.documentElement;
     this.userToken = localStorage.getItem('token')
     this.sharedHeaderService.roleSubject.subscribe((data: any)=>{
       this.role = data;
@@ -42,5 +47,37 @@ export class HeaderComponent implements OnInit{
 
   navigateToDashboard(){
     this.router.navigate(['/dashboard']);
+  }
+
+  openFullscreen() {
+    this.isFullScreen = true;
+    if (this.elem.requestFullscreen()) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen()) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen()) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen()) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
+  }
+/* Close fullscreen */
+  closeFullscreen() {
+    this.isFullScreen = false;
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
   }
 }
